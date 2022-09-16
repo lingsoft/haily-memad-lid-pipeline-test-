0971cf018e6f6fdad14150ab4dcfb34ce2f23f4b
LizaLing <107042985+LizaLing@users.noreply.github.com>
2022-09-16 10:16:26 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,15 +19,14 @@ def predict(audio_save_path, lang_segments_path):
         data = dict(
             sorted(data.items(), key=lambda item: item[0].split('_')[3]))
 
-        results = [{
-            "start": 2 * i,
-            "end": 2 * i + 2,
-            "features": {
-                "lang": lang
-            }
-        } for i, (seg, lang) in enumerate(data.items())]
-
-        return {"spoken_language_identification": results}
+        annotations = {}
+        for i, (seg, lang) in enumerate(data.items()):
+            annot = {
+                "start": 2 * i,
+                "end": 2 * i + 2,
+                }
+            annotations.setdefault(lang, []).append(annot)
+        return annotations
 
     else:  # Segment audio according to annotation and predict
         rc = subprocess.check_call([
@@ -38,14 +37,12 @@ def predict(audio_save_path, lang_segments_path):
         with open(os.path.join(cur_dir, fname, 'predict',
                                'utt2lang.json')) as json_file:
             data = json.load(json_file)
-
-        results = [{
-            "start": float(seg.split('_')[0]) * 1.0 / 1000,
-            "end": float(seg.split('_')[1]) * 1.0 / 1000,
-            "features": {
-                "lang": lang,
-                "true_label": seg.split('_')[2]
-            }
-        } for seg, lang in data.items()]
-
-        return {"spoken_language_identification": results}
+        
+        annotations = {}
+        for seg, lang in data.items():
+            annot = {
+                "start": float(seg.split('_')[0]) * 1.0 / 1000,
+                "end": float(seg.split('_')[1]) * 1.0 / 1000,
+                }
+            annotations.setdefault(lang, []).append(annot)
+        return annotations
