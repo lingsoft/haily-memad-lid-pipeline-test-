57d467fc44cc7595988f7c1b093bd9685a7ba816
LizaLing <107042985+LizaLing@users.noreply.github.com>
2022-09-30 10:56:44 +0300

    Update multi_form_req.py

--- multi_form_req.py
+++ multi_form_req.py
@@ -1,19 +1,28 @@
 import os
 import json
+from sys import exit
 import requests
-
+from elg.model import Failure
+from elg.model.base import StandardMessages
 
 def send_request(url, audio, anno=''):
 
     if anno:
         with open(anno) as json_file:
             anno_lst = json.load(json_file)
-
-        for anno_ in anno_lst:
-            anno_["features"] = {"label": anno_.pop("id") if "id" in anno_ else "x-nolang"}
-            anno_["start"] = float(anno_["start"])
-            anno_["end"] = float(anno_["end"])
-            anno_ = json.dumps(anno_)
+        
+        try:
+            for anno_ in anno_lst:
+                anno_["features"] = {"label": anno_.pop("id") if "id" in anno_ else "x-nolang"}
+                anno_["start"] = float(anno_["start"])
+                anno_["end"] = float(anno_["end"])
+                anno_ = json.dumps(anno_)
+        except Exception:
+            err_msg = StandardMessages.generate_elg_request_invalid(
+                        detail={"request": "Annotation JSON must contain starting and ending times of speech fragments."})
+            print(Failure(errors=[err_msg]))
+            exit()
+            
         annots = {"lang_segments": anno_lst}
         payload = {
             "type": "audio",
